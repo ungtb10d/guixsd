94730c7a6ad5c197e741e0065a0fca0a7fc4b7b4
Marius Bakke <marius@gnu.org>
2020-06-14 17:21:23 +0200

    gnu: docbook-xsl: Update to 1.79.2.
    * gnu/packages/patches/docbook-xsl-support-old-url.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Adjust accordingly.
    * gnu/packages/patches/docbook-xsl-nonrecursive-string-subst.patch: Adjust URL
    and context.
    * gnu/packages/docbook.scm (docbook-xsl): Update to 1.79.2.
    [source](uri): Adjust for new download location.

--- gnu/local.mk
+++ gnu/local.mk
@@ -968,6 +968,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/disarchive-cross-compilation.patch	\
   %D%/packages/patches/dkimproxy-add-ipv6-support.patch		\
   %D%/packages/patches/docbook-xsl-nonrecursive-string-subst.patch	\
+  %D%/packages/patches/docbook-xsl-support-old-url.patch	\
   %D%/packages/patches/doc++-include-directives.patch		\
   %D%/packages/patches/doc++-segfault-fix.patch			\
   %D%/packages/patches/docker-fix-tests.patch			\
