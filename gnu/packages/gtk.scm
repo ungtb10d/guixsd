63904adcbc343e82a7b9b597d7b16d708406d76c
Raghav Gururajan <rg@raghavgururajan.name>
2021-03-11 11:00:10 -0500

    gnu: gdk-pixbuf: Disable failing tests.
    * gnu/packages/gtk.scm (gdk-pixbuf) [arguments]<#:phases>['disable-failing-tests]: New phase.
    
    Signed-off-by: Léo Le Bouter <lle-bout@zaclys.net>

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -612,6 +612,12 @@ highlighting and other features typical of a source code editor.")
                   (string-append (assoc-ref inputs "docbook-xml")
                                  "/xml/dtd/docbook/"))))
              #t))
+         (add-before 'configure 'disable-failing-tests
+           (lambda _
+             (substitute* "tests/meson.build"
+               (("\\[ 'pixbuf-fail', \\['conform', 'slow'\\], \\],")
+                ""))
+             #t))
          ;; The slow tests take longer than the specified timeout.
          ,@(if (any (cute string=? <> (%current-system))
                     '("armhf-linux" "aarch64-linux"))
