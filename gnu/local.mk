8520d257f9ebacd4af6a54afe85c4921c809243c
Raghav Gururajan <rg@raghavgururajan.name>
2021-03-11 07:22:07 -0500

    gnu: glib: Update to 2.68.0.
    * gnu/packages/glib.scm (glib) [version]: Update to 2.68.0.
    [patches]: Remove glib-tests-timer.patch.
    [arguments]<#:phases>['patch-python-references]: New phase.
    ['pre-check]: New phase.
    ['move-bin]: New phase.
    ['patch-dbus-launch-path]: Remove phase.
    ['patch-gio-launch-desktop]: Remove phase.
    ['pre-build]: Remove phase.
    ['disable-failing-tests]: Modify phase.
    ['check]: Remove phase.
    ['move-executables]: Remove phase.
    [native-inputs]: Add intltool, python, libxml2 and libxslt.
    Remove dbus.
    [inputs]: Add bash-completion, dbus and libelf.
    [propagated-inputs]: Add libselinux.
    * gnu/packages/patches/glib-tests-timer.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Adjust.
    
    Signed-off-by: Léo Le Bouter <lle-bout@zaclys.net>

--- gnu/local.mk
+++ gnu/local.mk
@@ -1114,7 +1114,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/ghostscript-no-header-creationdate.patch \
   %D%/packages/patches/giara-fix-login.patch                      \
   %D%/packages/patches/glib-appinfo-watch.patch			\
-  %D%/packages/patches/glib-tests-timer.patch			\
   %D%/packages/patches/glibc-CVE-2018-11236.patch		\
   %D%/packages/patches/glibc-CVE-2018-11237.patch		\
   %D%/packages/patches/glibc-CVE-2019-7309.patch		\
