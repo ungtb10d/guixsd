attila-lendvai-patches/guix

build-system/meson: Fix cross-building to riscv64-linux.
* guix/build-system/meson.scm (make-machine-list): Add entries in
cpu_family and endian for riscv64-linux.

guix/build-system/meson.scm
@@ -58,6 +58,7 @@ for TRIPLET."
                           (if (target-64bit? triplet)
                               "ppc64"
                               "ppc"))
+                         ((target-riscv? triplet) "riscv64")
                          (#t (error "meson: unknown architecture"))))
     (cpu . ,(cond ((target-x86-32? triplet) ; i386, ..., i686
                    (substring triplet 0 4))
@@ -78,6 +79,8 @@ for TRIPLET."
                      ;; At least in Guix.  Aarch64 and 32-bit arm
                      ;; have a big-endian mode as well.
                      ((target-arm? triplet) "little")
+                     ((target-ppc32? triplet) "big")
+                     ((target-riscv? triplet) "little")
                      (#t (error "meson: unknown architecture"))))))
 
 (define (make-binaries-alist triplet)

ungtb10d/guixsd

guix/build-system/meson.scm
@@ -58,6 +58,7 @@ for TRIPLET."
                           (if (target-64bit? triplet)
                               "ppc64"
                               "ppc"))
+                         ((target-riscv64? triplet) "riscv64")
                          (#t (error "meson: unknown architecture"))))
     (cpu . ,(cond ((target-x86-32? triplet) ; i386, ..., i686
                    (substring triplet 0 4))
@@ -78,6 +79,8 @@ for TRIPLET."
                      ;; At least in Guix.  Aarch64 and 32-bit arm
                      ;; have a big-endian mode as well.
                      ((target-arm? triplet) "little")
+                     ((target-ppc32? triplet) "big")
+                     ((target-riscv64? triplet) "little")
                      (#t (error "meson: unknown architecture"))))))
 
 (define (make-binaries-alist triplet)