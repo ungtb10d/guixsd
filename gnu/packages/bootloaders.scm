attila-lendvai-patches/guix

gnu: dtc: Build without valgrind on riscv64-linux.
* gnu/packages/bootloaders.scm (dtc)[native-inputs]: When building for
riscv64-linux don't use valgrind.

gnu/packages/bootloaders.scm
@@ -454,7 +454,9 @@ menu to select one of the installed operating systems.")
        ("libyaml" ,libyaml)
        ("pkg-config" ,pkg-config)
        ("swig" ,swig)
-       ("valgrind" ,valgrind)))
+       ,@(if (target-riscv?)
+           '()
+           `(("valgrind" ,valgrind)))))
     (inputs
      `(("python" ,python)))
     (arguments

ungtb10d/guixsd

gnu/packages/bootloaders.scm
@@ -460,7 +460,9 @@ menu to select one of the installed operating systems.")
        ("libyaml" ,libyaml)
        ("pkg-config" ,pkg-config)
        ("swig" ,swig)
-       ("valgrind" ,valgrind)))
+       ,@(if (target-riscv64?)
+           '()
+           `(("valgrind" ,valgrind)))))
     (inputs
      `(("python" ,python)))
     (arguments