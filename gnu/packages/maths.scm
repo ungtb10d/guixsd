attila-lendvai-patches/guix

gnu: lapack: Fix building on riscv64-linux.
* gnu/packages/maths.scm (lapack)[inputs]: When building for
riscv64-linux explicitly add gfortran:lib.

gnu/packages/maths.scm
@@ -892,6 +892,9 @@ large scale eigenvalue problems.")
     (build-system cmake-build-system)
     (home-page "http://www.netlib.org/lapack/")
     (inputs `(("fortran" ,gfortran)
+              ,@(if (target-riscv?)
+                  `(("fortran:lib" ,gfortran "lib"))
+                  '())
               ("python" ,python-wrapper)))
     (arguments
      `(#:configure-flags (list

ungtb10d/guixsd

gnu/packages/maths.scm
@@ -903,6 +903,9 @@ large scale eigenvalue problems.")
     (build-system cmake-build-system)
     (home-page "http://www.netlib.org/lapack/")
     (inputs `(("fortran" ,gfortran)
+              ,@(if (target-riscv64?)
+                  `(("fortran:lib" ,gfortran "lib"))
+                  '())
               ("python" ,python-wrapper)))
     (arguments
      `(#:configure-flags (list