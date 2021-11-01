attila-lendvai-patches/guix

gnu: qemu: Skip some tests on riscv64-linux.
* gnu/packages/virtualization.scm (qemu)[arguments]: Add phase for
riscv64-linux to skip some unsupported tests.

gnu/packages/virtualization.scm
@@ -207,6 +207,18 @@
                ;; fails within the build environment.
                ((".*'test-char':.*" all)
                 (string-append "# " all)))))
+         ,(if (target-riscv?)
+            `(add-after 'unpack 'disable-some-tests
+               (lambda _
+                 ;; qemu.qmp.QMPConnectError: Unexpected empty reply from server
+                 (delete-file "tests/qemu-iotests/040")
+                 (delete-file "tests/qemu-iotests/041")
+                 (delete-file "tests/qemu-iotests/256")
+
+                 ;; No 'PCI' bus found for device 'virtio-scsi-pci'
+                 (delete-file "tests/qemu-iotests/127")
+                 (delete-file "tests/qemu-iotests/267")))
+            `())
          (add-after 'patch-source-shebangs 'patch-embedded-shebangs
            (lambda _
              ;; Ensure the executables created by these source files reference

ungtb10d/guixsd

gnu/packages/virtualization.scm
@@ -212,6 +212,18 @@
                ;; fails within the build environment.
                ((".*'test-char':.*" all)
                 (string-append "# " all)))))
+         ,(if (target-riscv64?)
+            `(add-after 'unpack 'disable-some-tests
+               (lambda _
+                 ;; qemu.qmp.QMPConnectError: Unexpected empty reply from server
+                 (delete-file "tests/qemu-iotests/040")
+                 (delete-file "tests/qemu-iotests/041")
+                 (delete-file "tests/qemu-iotests/256")
+
+                 ;; No 'PCI' bus found for device 'virtio-scsi-pci'
+                 (delete-file "tests/qemu-iotests/127")
+                 (delete-file "tests/qemu-iotests/267")))
+            `())
          (add-after 'patch-source-shebangs 'patch-embedded-shebangs
            (lambda _
              ;; Ensure the executables created by these source files reference