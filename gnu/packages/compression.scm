attila-lendvai-patches/guix

gnu: lz4: Build on riscv64-linux without valgrind.
* gnu/packages/compression.scm (lz4)[native-inputs]: Don't use valgrind
when building for riscv64-linux.

gnu/packages/compression.scm
@@ -819,7 +819,9 @@ decompression of some loosely related file formats used by Microsoft.")
     (native-inputs
      `(;; For tests.
        ("python" ,python)
-       ("valgrind" ,valgrind)))
+       ,@(if (target-riscv?)
+           '()
+           `(("valgrind" ,valgrind)))))
     (arguments
      `(;; Not designed for parallel testing.
        ;; See https://github.com/lz4/lz4/issues/957#issuecomment-737419821

ungtb10d/guixsd

gnu/packages/compression.scm
@@ -819,7 +819,9 @@ decompression of some loosely related file formats used by Microsoft.")
     (native-inputs
      `(;; For tests.
        ("python" ,python)
-       ("valgrind" ,valgrind)))
+       ,@(if (target-riscv64?)
+           '()
+           `(("valgrind" ,valgrind)))))
     (arguments
      `(;; Not designed for parallel testing.
        ;; See https://github.com/lz4/lz4/issues/957#issuecomment-737419821