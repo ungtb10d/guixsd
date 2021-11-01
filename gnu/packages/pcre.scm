attila-lendvai-patches/guix

gnu: pcre2: Fix building on riscv64-linux.
* gnu/packages/pcre.scm (pcre2)[arguments]: Adjust configure-flags to not
build with JIT when building for riscv64-linux.

gnu/packages/pcre.scm
@@ -110,7 +110,9 @@ POSIX regular expression API.")
                           "--enable-pcre2-16"
                           "--enable-pcre2-32"
                           ;; pcre2_jit_test fails on powerpc32.
-                          ,@(if (target-ppc32?)
+                          ;; JIT isn't supported yet on riscv64.
+                          ,@(if (or (target-ppc32?)
+                                    (target-riscv?))
                               '()
                               `("--enable-jit"))
                           "--disable-static")

ungtb10d/guixsd

gnu/packages/pcre.scm
@@ -112,7 +112,9 @@ POSIX regular expression API.")
                           "--enable-pcre2-16"
                           "--enable-pcre2-32"
                           ;; pcre2_jit_test fails on powerpc32.
-                          ,@(if (target-ppc32?)
+                          ;; JIT isn't supported yet on riscv64.
+                          ,@(if (or (target-ppc32?)
+                                    (target-riscv64?))
                               '()
                               `("--enable-jit"))
                           "--disable-static")