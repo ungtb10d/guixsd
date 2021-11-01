attila-lendvai-patches/guix

gnu: Use libunwind-next for riscv64-linux.
* gnu/packages/cpp.scm (folly)[inputs]: Use libunwind-next in place of
libunwind on riscv64-linux.
* gnu/packages/finance.scm (monero)[inputs],
* gnu/packages/freedesktop.scm (weston)[inputs],
* gnu/packages/gnome.scm (geary)[inputs],
* gnu/packages/linux.scm (perf)[inputs],
* gnu/packages/profiling.scm (make-scorep)[inputs],
* gnu/packages/prolog.scm (swi-prolog)[inputs],
* gnu/packages/web.scm (lighttpd)[inputs]: Same.

gnu/packages/cpp.scm
@@ -868,7 +868,9 @@ standard GNU style syntax for options.")
        ("libevent" ,libevent)
        ("libiberty" ,libiberty)
        ("libsodium" ,libsodium)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("lz4" ,lz4)
        ("openssl" ,openssl)
        ("snappy" ,snappy)

gnu/packages/finance.scm
@@ -683,7 +683,9 @@ other machines/servers.  Electroncash does not download the Bitcoin Cash blockch
        ("expat" ,expat)
        ("hidapi" ,hidapi)
        ("libsodium" ,libsodium)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("libusb" ,libusb)
        ("miniupnpc" ,miniupnpc)
        ("openssl" ,openssl)

gnu/packages/freedesktop.scm
@@ -1099,7 +1099,9 @@ protocol either in Wayland core, or some other protocol in wayland-protocols.")
        ("libinput" ,libinput-minimal)
        ("libjpeg" ,libjpeg-turbo)
        ("libpng" ,libpng)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("libva" ,libva)
        ("libwebp" ,libwebp)
        ("libx11" ,libx11)

gnu/packages/gnome.scm
@@ -11413,7 +11413,9 @@ join_paths\\('build-aux', 'post_install.py'\\)\\)")
        ("libhandy" ,libhandy-0.0)
        ("libpeas" ,libpeas)
        ("libsecret" ,libsecret)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("sqlite" ,sqlite)
        ("webkitgtk" ,webkitgtk)
        ("ytnef" ,ytnef)))

gnu/packages/linux.scm
@@ -4439,7 +4439,9 @@ in a digital read-out.")
        ("python" ,python-2)                    ;'perf' links against libpython
        ("elfutils" ,elfutils)
        ("libiberty" ,libiberty)      ;used alongside BDF for symbol demangling
-       ("libunwind" ,libunwind)      ;better stack walking
+       ,(if (target-riscv?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))           ;better stack walking
        ("numactl" ,numactl)          ;for 'perf bench numa mem'
 
        ;; Documentation.

gnu/packages/profiling.scm
@@ -348,7 +348,9 @@ different kinds of performance behavior.")
      `(("mpi" ,mpi)
        ("papi" ,papi)
        ("opari2" ,opari2)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("otf2" ,otf2)
        ("cubelib" ,cube "lib")                    ;for lib, include
        ("openmpi" ,openmpi)

gnu/packages/prolog.scm
@@ -23,6 +23,7 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix utils)
   #:use-module (guix packages)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system gnu)
@@ -121,7 +122,9 @@ manner.  It also features an interactive interpreter.")
        ("readline" ,readline)
        ("texinfo" ,texinfo)
        ("libarchive" ,libarchive)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("libjpeg" ,libjpeg-turbo)
        ("libxft" ,libxft)
        ("fontconfig" ,fontconfig)

gnu/packages/web.scm
@@ -874,7 +874,9 @@ stream.  Remote control of the module is possible over HTTP.")
     (inputs
      `(("cyrus-sasl" ,cyrus-sasl)
        ("libev" ,libev)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("linux-pam" ,linux-pam)
        ("mit-krb5" ,mit-krb5)
        ("openldap" ,openldap)

ungtb10d/guixsd

gnu/packages/cpp.scm
@@ -889,7 +889,9 @@ standard GNU style syntax for options.")
        ("libevent" ,libevent)
        ("libiberty" ,libiberty)
        ("libsodium" ,libsodium)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv64?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("lz4" ,lz4)
        ("openssl" ,openssl)
        ("snappy" ,snappy)

gnu/packages/finance.scm
@@ -688,7 +688,9 @@ other machines/servers.  Electroncash does not download the Bitcoin Cash blockch
        ("expat" ,expat)
        ("hidapi" ,hidapi)
        ("libsodium" ,libsodium)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv64?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("libusb" ,libusb)
        ("miniupnpc" ,miniupnpc)
        ("openssl" ,openssl)

gnu/packages/freedesktop.scm
@@ -1123,7 +1123,9 @@ protocol either in Wayland core, or some other protocol in wayland-protocols.")
        ("libinput" ,libinput-minimal)
        ("libjpeg" ,libjpeg-turbo)
        ("libpng" ,libpng)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv64?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("libva" ,libva)
        ("libwebp" ,libwebp)
        ("libx11" ,libx11)

gnu/packages/gnome.scm
@@ -11635,7 +11635,9 @@ join_paths\\('build-aux', 'post_install.py'\\)\\)")
        ("libhandy" ,libhandy-0.0)
        ("libpeas" ,libpeas)
        ("libsecret" ,libsecret)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv64?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("sqlite" ,sqlite)
        ("webkitgtk" ,webkitgtk)
        ("ytnef" ,ytnef)))

gnu/packages/linux.scm
@@ -4595,7 +4595,9 @@ in a digital read-out.")
        ("python" ,python-2)                    ;'perf' links against libpython
        ("elfutils" ,elfutils)
        ("libiberty" ,libiberty)      ;used alongside BDF for symbol demangling
-       ("libunwind" ,libunwind)      ;better stack walking
+       ,(if (target-riscv64?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))           ;better stack walking
        ("numactl" ,numactl)          ;for 'perf bench numa mem'
 
        ;; Documentation.

gnu/packages/profiling.scm
@@ -348,7 +348,9 @@ different kinds of performance behavior.")
      `(("mpi" ,mpi)
        ("papi" ,papi)
        ("opari2" ,opari2)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv64?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("otf2" ,otf2)
        ("cubelib" ,cube "lib")                    ;for lib, include
        ("openmpi" ,openmpi)

gnu/packages/prolog.scm
@@ -23,6 +23,7 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix utils)
   #:use-module (guix packages)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system gnu)
@@ -121,7 +122,9 @@ manner.  It also features an interactive interpreter.")
        ("readline" ,readline)
        ("texinfo" ,texinfo)
        ("libarchive" ,libarchive)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv64?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("libjpeg" ,libjpeg-turbo)
        ("libxft" ,libxft)
        ("fontconfig" ,fontconfig)

gnu/packages/web.scm
@@ -874,7 +874,9 @@ stream.  Remote control of the module is possible over HTTP.")
     (inputs
      `(("cyrus-sasl" ,cyrus-sasl)
        ("libev" ,libev)
-       ("libunwind" ,libunwind)
+       ,(if (target-riscv64?)
+          `("libunwind" ,libunwind-next)
+          `("libunwind" ,libunwind))
        ("linux-pam" ,linux-pam)
        ("mit-krb5" ,mit-krb5)
        ("openldap" ,openldap)