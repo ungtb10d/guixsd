attila-lendvai-patches/guix

gnu: php: Skip tests failing on riscv64-linux.
* gnu/packages/php.scm (php)[arguments]: Adjust custom 'prepare-tests
phase to delete some tests when building for riscv64-linux.

gnu/packages/php.scm
@@ -5,6 +5,7 @@
 ;;; Copyright © 2018 Ricardo Wurmus <rekado@elephly.net>
 ;;; Copyright © 2019 Oleg Pykhalov <go.wigust@gmail.com>
 ;;; Copyright © 2020 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;; Copyright © 2021 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -196,6 +197,14 @@
                                 "sapi/cli/tests/upload_2G.phpt"
                                 "Zend/tests/concat_003.phpt")))
                    '())
+             ,@(if (target-riscv?)
+                 ;; Drop tests known to fail on riscv64.
+                 '((for-each delete-file
+                             (list
+                               ;; extensive backtracking
+                               "ext/fileinfo/tests/cve-2014-3538-nojit.phpt"
+                               "ext/fileinfo/tests/cve-2014-3538.phpt")))
+                 '())
 
              ,@(if (target-ppc64le?)
                    ;; Drop tests known to fail on powerpc64le.

ungtb10d/guixsd

gnu/packages/php.scm
@@ -5,6 +5,7 @@
 ;;; Copyright © 2018 Ricardo Wurmus <rekado@elephly.net>
 ;;; Copyright © 2019 Oleg Pykhalov <go.wigust@gmail.com>
 ;;; Copyright © 2020 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;; Copyright © 2021 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -196,6 +197,14 @@
                                 "sapi/cli/tests/upload_2G.phpt"
                                 "Zend/tests/concat_003.phpt")))
                    '())
+             ,@(if (target-riscv64?)
+                 ;; Drop tests known to fail on riscv64.
+                 '((for-each delete-file
+                             (list
+                               ;; extensive backtracking
+                               "ext/fileinfo/tests/cve-2014-3538-nojit.phpt"
+                               "ext/fileinfo/tests/cve-2014-3538.phpt")))
+                 '())
 
              ,@(if (target-ppc64le?)
                    ;; Drop tests known to fail on powerpc64le.