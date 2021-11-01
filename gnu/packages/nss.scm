attila-lendvai-patches/guix

gnu: nss: Adjust make-flags for riscv64-linux.
* gnu/packages/nss.scm (nss)[arguments]: When building for riscv64-linux
don't build the binaries with gtest.

gnu/packages/nss.scm
@@ -1,7 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2013, 2014, 2015, 2016, 2017, 2018, 2019 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019 Mark H Weaver <mhw@netris.org>
-;;; Copyright © 2016, 2017, 2018, 2019 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2016, 2017, 2018, 2019, 2021 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2017, 2018 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2020, 2021 Marius Bakke <marius@gnu.org>
 ;;; Copyright © 2020 Jonathan Brielmaier <jonathan.brielmaier@web.de>
@@ -108,6 +108,11 @@ in the Mozilla clients.")
          (list "-C" "nss" (string-append "PREFIX=" out)
                "NSDISTMODE=copy"
                "NSS_USE_SYSTEM_SQLITE=1"
+               ;; The gtests fail to compile on riscv64.
+               ;; Skipping them doesn't affect the test suite.
+               ,@(if (target-riscv?)
+                   `("NSS_DISABLE_GTESTS=1")
+                   '())
                (string-append "NSPR_INCLUDE_DIR=" nspr "/include/nspr")
                ;; Add $out/lib/nss to RPATH.
                (string-append "RPATH=" rpath)

ungtb10d/guixsd

gnu/packages/nss.scm
@@ -1,7 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2013, 2014, 2015, 2016, 2017, 2018, 2019 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019 Mark H Weaver <mhw@netris.org>
-;;; Copyright © 2016, 2017, 2018, 2019 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2016, 2017, 2018, 2019, 2021 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2017, 2018 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2020, 2021 Marius Bakke <marius@gnu.org>
 ;;; Copyright © 2020 Jonathan Brielmaier <jonathan.brielmaier@web.de>
@@ -134,6 +134,11 @@ in the Mozilla clients.")
          (list "-C" "nss" (string-append "PREFIX=" out)
                "NSDISTMODE=copy"
                "NSS_USE_SYSTEM_SQLITE=1"
+               ;; The gtests fail to compile on riscv64.
+               ;; Skipping them doesn't affect the test suite.
+               ,@(if (target-riscv64?)
+                   `("NSS_DISABLE_GTESTS=1")
+                   '())
                (string-append "NSPR_INCLUDE_DIR=" nspr "/include/nspr")
                ;; Add $out/lib/nss to RPATH.
                (string-append "RPATH=" rpath)