attila-lendvai-patches/guix

gnu: libfaketime: Adjust flags on riscv64-linux.
* gnu/packages/check.scm (libfaketime)[arguments]: When building for
riscv64-linux adjust CFLAGS to make the package work.

gnu/packages/check.scm
@@ -2755,7 +2755,8 @@ portable to just about any platform.")
                         ;; indefinitely.  See README.packagers for more information.
                         ;; There are specific instructions to not enable more flags
                         ;; than absolutely needed.
-                        ,(if (target-ppc64le?)
+                        ,(if (or (target-ppc64le?)
+                                 (target-riscv?))
                            `(setenv "FAKETIME_COMPILE_CFLAGS"
                                     "-DFORCE_MONOTONIC_FIX -DFORCE_PTHREAD_NONVER")
                            `(setenv "FAKETIME_COMPILE_CFLAGS"

ungtb10d/guixsd

gnu/packages/check.scm
@@ -2860,7 +2860,8 @@ portable to just about any platform.")
                         ;; indefinitely.  See README.packagers for more information.
                         ;; There are specific instructions to not enable more flags
                         ;; than absolutely needed.
-                        ,(if (target-ppc64le?)
+                        ,(if (or (target-ppc64le?)
+                                 (target-riscv64?))
                            `(setenv "FAKETIME_COMPILE_CFLAGS"
                                     "-DFORCE_MONOTONIC_FIX -DFORCE_PTHREAD_NONVER")
                            `(setenv "FAKETIME_COMPILE_CFLAGS"