cc4fbf7f1c6625653be2cf6023bc9f52adcd7f11
Lars-Dominik Braun <lars@6xq.net>
2021-12-31 15:33:21 +0100

    gnu: python-eliot: Remove redundant input.
    * gnu/packages/python-xyz.scm (python-eliot)[native-inputs]: Remove
    python-setuptools.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -25954,7 +25954,6 @@ tbutils
            python-flake8
            python-hypothesis
            python-pytest
-           python-setuptools
            python-sphinx
            python-sphinx-rtd-theme
            python-testtools
