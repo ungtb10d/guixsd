b57ce353a9279faad99012017d289e99bca21733
Lars-Dominik Braun <lars@6xq.net>
2022-01-07 13:41:34 +0100

    gnu: python-argcomplete: Add missing test input.
    * gnu/packages/python-xyz.scm (python-argcomplete)[native-inputs]:
    Add python-pip.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -18162,6 +18162,7 @@ multitouch applications.")
      (list python-coverage
            python-flake8
            python-pexpect
+           python-pip
            python-wheel
            tcsh
            fish
